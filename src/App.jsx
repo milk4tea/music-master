9bd128ec41fddcef6d084cd29f978a30afb8f8f6
Jeremy Feng <jeremy.jfeng@gmail.com>
2017-08-04 10:48:00 +1000

    init music app

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  authorize() {
+    const BASE_URL = "https://accounts.spotify.com/authorize?";
+    const CLIENT_ID = ""
+    const AUTHORIZE_URL = `${BASE_URL}`
+
+  }
+
+  search() {
+    const BASE_URL = "https://api.spotify.com/v1/search?";
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log('FETCH_URL', FETCH_URL);
+  }
+  render() {
+    return(
+      <div className="App">
+        <div className="App-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search an artist..."
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if(event.key === 'Enter') {
+                  this.search()
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

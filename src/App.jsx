c8135587423476264fed2897e59b8785717f0398
Jeremy Feng <jeremy.jfeng@gmail.com>
2017-08-04 12:10:07 +1000

    init docs dir

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,33 @@ class App extends Component {
 
   authorize() {
     const BASE_URL = "https://accounts.spotify.com/authorize?";
-    const CLIENT_ID = ""
-    const AUTHORIZE_URL = `${BASE_URL}`
+    const CLIENT_ID = "e43af2886eae425caeaa7c5891197b2b"
+    const AUTHORIZE_URL = `${BASE_URL}client_id=${CLIENT_ID}&response_type=code&redirect_uri=https://milk4tea.github.io/music-master-prod/`
+    let headers = new Headers();
 
+    const auth = fetch(AUTHORIZE_URL, {
+      method: 'GET',
+      headers: headers,
+    })
+    // .then(response => response.json())
+    // .then(json => console.log('json', json))
+    .then(response => console.log(response))
+    .catch(error => console.log('error', error));
+    console.log(auth);
   }
 
   search() {
+    this.authorize();
     const BASE_URL = "https://api.spotify.com/v1/search?";
     const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    console.log('FETCH_URL', FETCH_URL);
+
+    // fetch(FETCH_URL, {
+    //   method: 'GET'
+    // })
+    // .then(response => response.json())
+    // .then(json => console.log('json', json));
   }
+
   render() {
     return(
       <div className="App">
